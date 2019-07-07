83016e357503b9144d67200168b6b5dc3cb4dcf3
Yu Software <yuusoftware@gmail.com>
2019-07-07 11:33:02 +0100

    Initial Commit

---
+++ ProgressMaskView/ProgressMaskView.h
@@ -0,0 +1,19 @@
+//
+//  ProgressMaskView.h
+//  ProgressMaskView
+//
+//  Created by Yu Software on 2019/07/07.
+//  Copyright © 2019 Yu Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ProgressMaskView.
+FOUNDATION_EXPORT double ProgressMaskViewVersionNumber;
+
+//! Project version string for ProgressMaskView.
+FOUNDATION_EXPORT const unsigned char ProgressMaskViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProgressMaskView/PublicHeader.h>
+
+
---
+++ ProgressMaskViewTests/ProgressMaskViewTests.swift
@@ -0,0 +1,34 @@
+//
+//  ProgressMaskViewTests.swift
+//  ProgressMaskViewTests
+//
+//  Created by Yu Software on 2019/07/07.
+//  Copyright © 2019 Yu Software. All rights reserved.
+//
+
+import XCTest
+@testable import ProgressMaskView
+
+class ProgressMaskViewTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
